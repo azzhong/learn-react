47a7e9061d82ff7dab469e6e318ef4c2a1971dee
Alfred Zhigang Zhong <az@azland>
2022-11-12 23:03:52 -0800

    created NewItem component version to handle adding items

--- az-todo-demo/src/App.js
+++ az-todo-demo/src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import { useState } from 'react'
 import TodoItem from './TodoItem';
 import ItemList from './ItemList';
+import NewItem from './NewItem';
 
 function App() {
 
+  // Hold item data
   const [todos, setTodos] = useState([
     { name: "Learn", done: true},
     { name: "Read", done: true},
@@ -15,8 +17,10 @@ function App() {
     { name: "Chill", done: true},
   ]);
 
+  // For new task 
   const [item, setItem] = useState('');
 
+  // For new task 
   const addClickedFunc = () => {
     if (!item) {
       return;
@@ -26,6 +30,16 @@ function App() {
     setItem('');
   };
 
+  // for new task (component version)
+  const addItemFunc = (item) => {
+    if (!item) {
+      return;
+    }
+    const newTodos = todos.concat([{name: item, done: false}]);
+    setTodos(newTodos);
+  };
+
+  // For new task 
   const itemChangedFunc = (e) => {
     const itemName = e.target.value;
     // setItem({name: itemName, done: true});
@@ -49,10 +63,15 @@ function App() {
     <div className='App'>
       <h1>AZ Todo List Demo</h1>
       <div>Not done: {notDone}</div>
+
+      <h2>New Item without Component</h2>
       <div>
         <input onChange={itemChangedFunc} type='text' value={item}></input>
         <button onClick={addClickedFunc}>Add</button>
       </div>
+
+      <h2>New Item using Component</h2>
+      <NewItem addItemFunc={addItemFunc}></NewItem>
 
       <h2>Demo Using TodoList Component</h2>
       <ItemList todos={todos} itemToggleFunc={toggleChecked}/>
